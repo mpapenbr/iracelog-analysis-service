751d89f9459998dab9924a6c702dd9a225970946
mpapenbr <Markus.Papenbrock@gmail.com>
2021-09-03 17:14:34 +0200

    refactor: async methods

--- src/dbexchange.ts
+++ src/dbexchange.ts
@@ -14,7 +14,24 @@ export const processSomething = (eventId: number) => {
   });
 };
 
-export const storeAnalysisData = (eventKey: string, data: any) => {
+export const storeAnalysisData = async (eventKey: string, data: any) => {
+  let client = await pool.connect();
+  try {
+    await client.query("BEGIN");
+    let result = await client.query("select id from event where event_key=$1", [eventKey]);
+    const eventId = result.rows[0].id;
+    result = await client.query("select id from analysis where event_id=$1", [eventId]);
+    if (result.rows.length === 0) {
+      await client.query("insert into analysis (event_id,data) values ($1,$2::jsonb)", [eventId, JSON.stringify(data)]);
+    } else {
+      await client.query("update analysis set data = $2::jsonb where event_id=$1", [eventId, JSON.stringify(data)]);
+    }
+    await client.query("COMMIT");
+  } finally {
+    client.release();
+  }
+};
+export const storeAnalysisDataOld = (eventKey: string, data: any) => {
   pool.connect((err, client, done) => {
     const shouldAbort = (err: Error) => {
       if (err) {
@@ -100,7 +117,23 @@ export const getArchivedAnalysisSync = async (eventKey: string) => {
   return rows[0].data;
 };
 
-export const processEvent = (eventKey: string) => {
+export const processEvent = async (eventKey: string) => {
+  const client = await pool.connect();
+  await client.query("BEGIN");
+  try {
+    let res = await client.query("select * from event where event_key=$1", [eventKey]);
+    const manifests = createManifests(res.rows[0].data.manifests);
+    const eventId = res.rows[0].id;
+    res = await client.query("select * from wampdata where event_id=$1 order by data->'timestamp' asc ", [eventId]);
+    const proc = new BulkProcessor(manifests);
+    const procResult = proc.process(res.rows.map((v) => v.data));
+    await client.query("COMMIT");
+    return procResult;
+  } finally {
+    client.release();
+  }
+};
+export const processEventOld = (eventKey: string) => {
   return new Promise((resolve, reject) => {
     pool.query("select * from event where event_key=$1", [eventKey]).then((res) => {
       const manifests = createManifests(res.rows[0].data.manifests);
